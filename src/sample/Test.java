3d58494319042661b265e917183e13163649dc93
milos7250 <milos.micik7@gmail.com>
2021-03-19 14:31:35 +0000

    Add Test class

---
+++ src/sample/Test.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class Test {
+    public static void main(String[] args) {
+        new Test();
+    }
+
+    private Test() {
+        initialize();
+        process();
+    }
+
+    private void initialize() {
+
+    }
+
+    private void process() {
+        testTranslator();
+    }
+
+    private void testTranslator() {
+
+    }
+}

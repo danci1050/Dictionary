396fb18b6815f633936d59b386e215880347fc19
milos7250 <milos.micik7@gmail.com>
2021-03-24 17:13:13 +0000

    Remove unnecessary import.

--- src/sample/Dictionary.java
+++ src/sample/Dictionary.java
@@ -7,7 +7,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 public class Dictionary {

96c8e878ddbb0c5d9df0bd1767545603ff8bbe6f
danci1050 <obilesa@gmai.com>
2021-04-05 16:32:56 +0200

    Fix commit merge

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		pStage=primaryStage;
+		Controller.setAddNewWord(true);
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
 		Parent root = loader.load();
 		controller = loader.getController();
